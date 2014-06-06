3d287aadcf357790b6b3583ce1784b7d67e10760
Тимофей Миронов <timirlan666@gmail.com>
2014-06-06 12:25:44 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Created by mta on 05.06.14.
+ */
+/*jslint node:true */
+/*global console */
+var exec_sync = require('exec-sync'),
+    path = require('path');
+
+console.log('Django loaded');
+var djangoManagepyRunner = function (config, baseDir) {
+    "use strict";
+    var python = config.virtualenvDir ? path.join(baseDir, config.virtualenvDir, 'bin/python') : 'python',
+        manage = path.join(baseDir, config.manageFile),
+        managepyCommand = [python, manage].join(' ');
+
+    function callManagepy(args) {
+        return exec_sync([managepyCommand, args].join(' '));
+    }
+
+    config.commands.forEach(function (command) {
+        if (config.silent) {
+            callManagepy(command);
+        } else {
+            console.log(callManagepy(command));
+        }
+    });
+};
+
+djangoManagepyRunner.$inject = ['config.djangoManage', 'config.basePath'];
+
+module.exports = {
+    'framework:django-manage': ['factory', djangoManagepyRunner]
+};
